c46e0c307e2d55f8eeb6c578a4dfd4445826adc4
Tobin Pomeroy <tobin.pomeroy@gmail.com>
2018-03-04 22:29:17 -0800

    Added group structure and PadView

---
+++ TappyFingers/TappyFingers/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TappyFingers
+//
+//  Created by Tobin Pomeroy on 3/4/18.
+//  Copyright © 2018 Tobin Pomeroy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
---
+++ TappyFingers/TappyFingers/Views/PadView.swift
@@ -0,0 +1,24 @@
+//
+//  PadView.swift
+//  TappyFingers
+//
+//  Created by Tobin Pomeroy on 3/4/18.
+//  Copyright © 2018 Tobin Pomeroy. All rights reserved.
+//
+
+import UIKit
+
+class PadView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func setup() {
+        
+    }
+}
