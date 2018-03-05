6366f0606127c1f61a1958b75afcb8e49ddba617
Tobin Pomeroy <tobin.pomeroy@gmail.com>
2018-03-04 22:54:15 -0800

    Added PadView class.

--- TappyFingers/TappyFingers/Views/PadView.swift
+++ TappyFingers/TappyFingers/Views/PadView.swift
@@ -7,18 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PadView: UIView {
     
+    var audioPlayer = AVAudioPlayer()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setup()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        setup()
     }
     
     func setup() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(padWasTapped))
+        self.addGestureRecognizer(tap)
+        self.isUserInteractionEnabled = true
         
+        let CatSound = NSURL(fileURLWithPath: Bundle.main.path(forResource: "Meow-sounds", ofType: "mp3")!)
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: CatSound as URL)
+            audioPlayer.prepareToPlay()
+        } catch {
+            print("Problem in getting File")
+        }
+    }
+    
+    @objc func padWasTapped() {
+        print("tapped!")
+        audioPlayer.play()
     }
 }
